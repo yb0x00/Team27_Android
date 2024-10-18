7f374da918225a55cc8c7a3404b3034b5c4920c9
yb0x00 <yeongbeen0x00@gmail.com>
2024-10-19 02:45:07 +0900

    Reapply "feat: 같이 찾기 화면 구성"
    This reverts commit a63de811929ba80f725c182d873592ff51fd6ee3.

---
+++ app/src/main/java/com/example/togetherpet/searching/SearchingViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.togetherpet.searching
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.togetherpet.testData.entity.Missing
+import com.example.togetherpet.testData.entity.User
+import com.example.togetherpet.testData.repository.MissingRepository
+import com.example.togetherpet.testData.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchingViewModel @Inject constructor(
+    private val missingRepository: MissingRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
+    private val _petName = MutableStateFlow<String?>(null)
+    val petName: StateFlow<String?> get() = _petName.asStateFlow()
+
+    //실종 동물 데이터
+    private val _missingPets = MutableStateFlow<List<Missing>>(emptyList())
+    val missingPets: StateFlow<List<Missing>> get() = _missingPets.asStateFlow()
+
+    init {
+        loadData()
+    }
+
+    fun loadData() {
+        viewModelScope.launch {
+            loadMissingPetData()
+            val userJob = async { loadUserData() }
+            val user = userJob.await()
+
+            _petName.value = user?.petName ?: "오류"
+        }
+    }
+
+    private suspend fun loadMissingPetData() {
+        val pets = missingRepository.getAllMissingPets()
+        _missingPets.emit(pets)
+    }
+
+    private suspend fun loadUserData(): User? {
+        return userRepository.getUserById(1)
+    }
+}
